36158aafdae3ecad9042102b224e8f1332acb2d9
winiymissl <winiymissl@163.com>
2023-12-23 21:24:10 +0800

    观察者模式

---
+++ src/designpattern/观察者模式/LiSi.java
@@ -0,0 +1,17 @@
+package designpattern.观察者模式;
+
+import java.util.Observable;
+import java.util.Observer;
+
+/**
+ * @Description
+ * @Author winiymissl
+ * @Date 2023-12-19
+ */
+public class LiSi implements Observer {
+
+    @Override
+    public void update(Observable o, Object arg) {
+
+    }
+}
